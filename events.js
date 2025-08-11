8b666126b75777a1857ad59c5c59c799776973f8
Renzo Xavier <renzxavi@gmail.com>
2025-08-10 21:20:35 -0300

    solution-click change

--- events.js
+++ events.js
@@ -1,3 +1,8 @@
 document.querySelector('.container').addEventListener('click', () => {
     alert('Hello! I am the div');
 });
+
+document.querySelector('button').addEventListener('click', (event) => {
+    event.stopPropagation();
+    alert('Hello! I am the button');
+});
